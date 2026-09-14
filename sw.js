bbda58ffa629f86dfd074e52baa57f1638964702
mina200112051119-sys <mina200112051119@gmail.com>
2026-09-14 14:13:34 +0900

    本試験モード反映用にService Workerのキャッシュを更新

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='boki1-v31';
+const CACHE='boki1-v32';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./app.html','./patch.js','./mistake-explanation.js','./advanced-questions.js','./real-exam-mode.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
