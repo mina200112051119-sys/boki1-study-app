51f40ca98c80bc1de53eb5c170b46206b2218b9d
mina200112051119-sys <mina200112051119@gmail.com>
2026-09-18 09:49:34 +0900

    有価証券本試験型問題を反映するためv47へ更新

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='boki1-v46';
+const CACHE='boki1-v47';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./app.html','./patch.js','./mistake-explanation.js','./advanced-questions.js','./real-exam-mode.js','./real-exam-safe.js','./study-flow.js','./deep-topic-quiz.js','./deep-topic-cases.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
